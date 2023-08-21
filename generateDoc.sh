0cb3ff29932dac5f67e5fa367faa5dbe7d01855c
박주철 (모빌리티앱개발TF) <park.jc@kt.com>
2023-08-21 10:37:00 +0900

    script수정

--- generateDoc.sh
+++ generateDoc.sh
@@ -2,10 +2,14 @@
 
 echo "generate documents"
 
-for f in *;
+for f in */;
  do
-    echo  "swift doc generate ./$f/Sources --module-name $f --output ./$f/Documentation --format html";
-    swift doc generate ./$f/Sources --module-name $f --output ./$f/Documentation --format html
+#    echo  "swift doc generate ./$f/Sources --module-name $f --output ./$f/Documentation --format html";
+    dir=$(echo $f | tr "/" "\n")
+    echo "--------"
+    echo "gen doc $dir"
+    swift doc generate ./$dir/Sources --module-name $dir --output ./$dir/Documentation --format html
+    echo "--------"
  done
 
 
