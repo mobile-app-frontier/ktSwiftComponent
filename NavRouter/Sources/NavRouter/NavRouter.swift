e4bb944a4588c2d37b11f028872975a1083600d7
박주철 (모빌리티앱개발TF) <park.jc@kt.com>
2023-08-23 17:13:17 +0900

    NavRouter 기능 변경

--- NavRouter/Sources/NavRouter/NavRouter.swift
+++ NavRouter/Sources/NavRouter/NavRouter.swift
@@ -1,6 +1,9 @@
 
 import SwiftUI
 
+/// Navigation Action
+/// navigation이 어떤 이벤트를 진행했는지 delegate로 전달하기 위한 model
+///
 public enum NavRouterAction{
     case start
     case pushRoute(pushedRoute: any NavRoute)
@@ -11,8 +14,12 @@ public enum NavRouterAction{
     case presentRouteSheet(presentedRoute: any NavRoute)
     case dimissRoute
     case replaceRoute(replaced: any NavRoute)
+    case replaceRouteTo(replaced: any NavRoute)
 }
 
+/// NavRouter
+/// SwiftUI 에서 UIKit의 ViewController 의 navigation방식을 사용 할 수 있도록
+/// 돕는 interface
 public protocol NavRouter: ObservableObject {
     associatedtype Route: NavRoute
     
@@ -29,7 +36,7 @@ public protocol NavRouter: ObservableObject {
     /// present modal or overlay screen
     func present(_ route: Route, animated: Bool , isModal: Bool)
     
-    func presentWithOptions(_ route: Route, animated: Bool, options:NavRoutePresentOptions)
+    func present(_ route: Route, animated: Bool, options:NavRoutePresentOptions)
     
     /// dismiss present screen
     /// present로 올라온 view만 dismiss 가능
@@ -37,7 +44,7 @@ public protocol NavRouter: ObservableObject {
     func dismiss(animated: Bool )
     
     /// navigation stack에 route를 추가
-    func push(_ route: Route, animated: Bool )
+    func push(_ route: Route, animated: Bool, options:NavRoutePresentOptions?)
     
     /// navigation stack에서 최 상위 vc를 제거
     func pop(animated: Bool )
@@ -49,8 +56,11 @@ public protocol NavRouter: ObservableObject {
     /// 지정한 restorationIdentifier 가 vc에 없을시 아무 동작 안함.
     func popToView(restorationIdentifier: String?, animated: Bool)
     
-    ///. navigation stack에 최 상위vc를 제거 하고 신규 vc를 추가
-    func replace(_ route: Route, animated: Bool)
+    /// navigation stack에 최 상위vc를 제거 하고 신규 vc를 추가
+    func replace(_ route: Route, animated: Bool, options:NavRoutePresentOptions?)
+    
+    /// navigation stack에 restorationIdentifier vc이후의 stack을 제거 하고 신규 vc를 추가
+    func replaceTo(_ route: Route, animated: Bool, restorationIdentifier: String?, options:NavRoutePresentOptions?)
     
     /// NavRouter delegate
     func didRouteNav(action: NavRouterAction)
