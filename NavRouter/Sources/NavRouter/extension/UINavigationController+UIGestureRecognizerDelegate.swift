84bb41ee74b9869f42fd251b15c2e98346beda21
박주철 (모빌리티앱개발TF) <park.jc@kt.com>
2023-08-31 14:28:54 +0900

    update hide in will appear

--- NavRouter/Sources/NavRouter/extension/UINavigationController+UIGestureRecognizerDelegate.swift
+++ NavRouter/Sources/NavRouter/extension/UINavigationController+UIGestureRecognizerDelegate.swift
@@ -11,11 +11,15 @@ extension UINavigationController: ObservableObject, UIGestureRecognizerDelegate 
     override open func viewDidLoad() {
         super.viewDidLoad()
         interactivePopGestureRecognizer?.delegate = self
-        self.isNavigationBarHidden = true
     }
 
     public func gestureRecognizerShouldBegin(_ gestureRecognizer: UIGestureRecognizer) -> Bool {
         return viewControllers.count > 1
+    }
+    
+    override open func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        self.isNavigationBarHidden = true
     }
 }
 
